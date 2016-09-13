3fc6e417e34ca69a3865824958fe98809abda9fb
Barry Jones <atirez@bitbucket.com>
2016-09-13 23:01:30 +0100

    added stringlengthvalidator and tests

---
+++ src/FormatValidator/Validators/StringLengthValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormatValidator.Validators
+{
+    public class StringLengthValidator
+    {
+        private int _maxLength;
+
+        public StringLengthValidator()
+        {
+            _maxLength = 0;
+        }
+
+        public bool IsValid(string toCheck)
+        {
+            return toCheck.Length <= MaxLength;
+        }
+
+        public int MaxLength
+        {
+            get
+            {
+                return _maxLength;
+            }
+            set
+            {
+                _maxLength = value;
+            }
+        }
+    }
+}
--- src/FormatValidatorTests/Class1.cs
+++ src/FormatValidatorTests/Class1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FormatValidator;
+using FormatValidator.Validators;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FormatValidatorTests
@@ -21,6 +22,50 @@ namespace FormatValidatorTests
             bool hasRequirements = validator.HasRequirments;
 
             Assert.IsTrue(hasRequirements);
+        }
+
+        [TestMethod]
+        public void StringLengthValidator_WhenStringIsLongerThanMaxLength_IsValid()
+        {
+            const string INPUT = "a test string";
+            const bool EXPECTED = false;
+
+            StringLengthValidator validator = new StringLengthValidator();
+            validator.MaxLength = 10;
+
+            bool result = validator.IsValid(INPUT);
+
+            Assert.AreEqual(EXPECTED, result);
+        }
+
+        [TestMethod]
+        public void StringLengthValidator_WhenStringIsMaxLength_IsValid()
+        {
+            const string INPUT = "a test string";
+            const bool EXPECTED = true;
+            const int MAX_LENGTH = 13;
+
+            StringLengthValidator validator = new StringLengthValidator();
+            validator.MaxLength = MAX_LENGTH;
+
+            bool result = validator.IsValid(INPUT);
+
+            Assert.AreEqual(EXPECTED, result);
+        }
+
+        [TestMethod]
+        public void StringLengthValidator_WhenStringIsSmaller_IsValid()
+        {
+            const string INPUT = "a test";
+            const bool EXPECTED = true;
+            const int MAX_LENGTH = 13;
+
+            StringLengthValidator validator = new StringLengthValidator();
+            validator.MaxLength = MAX_LENGTH;
+
+            bool result = validator.IsValid(INPUT);
+
+            Assert.AreEqual(EXPECTED, result);
         }
     }
 }
