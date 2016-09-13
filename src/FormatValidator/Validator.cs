32d5c5792e8a35c7f5aca2a7bc0b07019d9daef3
Barry Jones <atirez@bitbucket.com>
2016-09-13 22:51:26 +0100

    initial commit

---
+++ src/FormatValidator/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormatValidator
+{
+    public class Validator
+    {
+        private bool _hasRequirements;
+
+        public Validator()
+        {
+            _hasRequirements = false;
+        }
+
+        public void LoadRequirements(string fromFile)
+        {
+        }
+
+        public bool HasRequirments
+        {
+            get
+            {
+                return _hasRequirements;
+            }
+        }
+    }
+}
---
+++ src/FormatValidatorTests/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormatValidator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FormatValidatorTests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void Nothing()
+        {
+            string fromFile = string.Empty;
+
+            Validator validator = new Validator();
+            validator.LoadRequirements(fromFile);
+
+            bool hasRequirements = validator.HasRequirments;
+
+            Assert.IsTrue(hasRequirements);
+        }
+    }
+}
