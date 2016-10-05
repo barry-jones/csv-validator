792e89ce8355a36844d16b6f6feed45a10335237
Barry Jones <github@barryjones.me.uk>
2016-10-05 21:50:31 +0100

    added more json reader tests for checking conversion between json and configuration information

--- src/ValidateTests/Unit/JsonReaderTests.cs
+++ src/ValidateTests/Unit/JsonReaderTests.cs
@@ -31,5 +31,38 @@ namespace FormatValidatorTests.Unit
 
             Assert.AreEqual(EXPECTED, configuration.Columns[1].Name);
         }
+
+        [TestMethod]
+        public void JsonReader_WhenHasHeaderRowIsProvided_ShouldBePopulated()
+        {
+            const string INPUT = "{ \"hasHeaderRow\": true }";
+            JsonReader reader = new JsonReader();
+
+            ValidatorConfiguration configuration = reader.Read(INPUT);
+
+            Assert.AreEqual(true, configuration.HasHeaderRow);
+        }
+
+        [TestMethod]
+        public void JsonReader_WhenColumnSeperatorProvided_ShouldBePopulated()
+        {
+            const string INPUT = "{ \"columnSeperator\": \"|\" }";
+            JsonReader reader = new JsonReader();
+
+            ValidatorConfiguration configuration = reader.Read(INPUT);
+
+            Assert.AreEqual("|", configuration.ColumnSeperator);
+        }
+
+        [TestMethod]
+        public void JsonReader_WhenRowSeperatorProvided_ShouldBePopulated()
+        {
+            const string INPUT = "{ \"rowSeperator\": \"\\r\\n\" }";
+            JsonReader reader = new JsonReader();
+
+            ValidatorConfiguration configuration = reader.Read(INPUT);
+
+            Assert.AreEqual("\r\n", configuration.RowSeperator);
+        }
     }
 }
