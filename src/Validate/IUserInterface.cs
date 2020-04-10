4222a9e9c6f44cf58c3bd18bef11c40ce5164e06
Barry Jones <gitkraken@barryjones.me.uk>
2020-04-10 23:24:57 +0100

    converted to actual interface for ui classes

--- src/Validate/IUserInterface.cs
+++ src/Validate/IUserInterface.cs
@@ -1,7 +1,28 @@
 ﻿
+using System;
+using System.Collections.Generic;
+
 namespace FormatValidator
 {
-    public class IUserInterface
+    public interface IUserInterface
     {
+        /// <summary>
+        /// Display a start message
+        /// </summary>
+        public void ShowStart();
+
+        /// <summary>
+        /// Display an error for a reported row
+        /// </summary>
+        /// <param name="error"></param>
+        public void ReportRowError(RowValidationError error);
+
+        /// <summary>
+        /// Show the summary details for the validation
+        /// </summary>
+        /// <param name="validator">The validator used</param>
+        /// <param name="errors">The full list of errors</param>
+        /// <param name="duration">The duration of the validation</param>
+        public void ShowSummary(Validator validator, List<RowValidationError> errors, TimeSpan duration);
     }
 }
