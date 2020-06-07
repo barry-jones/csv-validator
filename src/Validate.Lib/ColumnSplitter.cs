8951017e887a4202c01e9aedf4b039a8b413f1df
Barry Jones <github@barryjones.me.uk>
2020-06-07 23:51:16 +0100

    added support for quoted strings in input files (#11)

---
+++ src/Validate.Lib/ColumnSplitter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FormatValidator
+{
+    /// <summary>
+    /// Seperates a string in to multiple columns based on seperator strings.
+    /// </summary>
+    internal class ColumnSplitter
+    {
+        /// <summary>
+        /// Splits a <paramref name="input"/> string in to seperate parts based on the provided
+        /// <paramref name="seperator"/>.
+        /// </summary>
+        /// <param name="input">The string to be seperated</param>
+        /// <param name="seperator">The seperator string to split on.</param>
+        /// <returns>An array of strings</returns>
+        public static string[] Split(string input, string seperator)
+        {
+            return Regex.Split(input,
+                $"{EscapeSeperator(seperator)}(?=([^\"]*\"[^\"]*\")*[^\"]*$)"
+                );
+        }
+
+        // escape all of the regex special characters
+        private static string EscapeSeperator(string seperator)
+        {
+            return seperator
+                .Replace(@"\", @"\\")
+                .Replace("^", @"\^")
+                .Replace("$", @"\$")
+                .Replace(".", @"\.")
+                .Replace("|", @"\|")
+                .Replace("?", @"\?")
+                .Replace("*", @"\*")
+                .Replace("+", @"\+")
+                .Replace("{", @"\{")
+                .Replace("[", @"\[")
+                .Replace("(", @"\(")
+                .Replace(")", @"\)");
+        }
+    }
+}
---
+++ test/ValidateTests/Unit/ColumnSplitterTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidateTests.Unit
+{
+    using FormatValidator;
+
+    [TestClass]
+    public class ColumnSplitterTests
+    {
+        [TestMethod]
+        public void Split_WhenOnlyOneColmn_ReturnsSingleResult()
+        {
+            const string INPUT = "just a string";
+            const string SEPERATOR = ",";
+
+            string[] result = ColumnSplitter.Split(INPUT, SEPERATOR);
+
+            Assert.AreEqual(result.Length, 1);
+            Assert.AreEqual(result[0], INPUT);
+        }
+
+        [TestMethod]
+        public void Split_WhenTwoColmns_ReturnsTwoResults()
+        {
+            const string INPUT = "just a string,seperated";
+            const string SEPERATOR = ",";
+
+            string[] result = ColumnSplitter.Split(INPUT, SEPERATOR);
+
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0], "just a string");
+            Assert.AreEqual(result[1], "seperated");
+        }
+
+        [TestMethod]
+        public void Split_WhenUsingMultiCharSeperators_IsCorrect()
+        {
+            const string INPUT = "just a string##seperated";
+            const string SEPERATOR = "##";
+
+            string[] result = ColumnSplitter.Split(INPUT, SEPERATOR);
+
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0], "just a string");
+            Assert.AreEqual(result[1], "seperated");
+        }
+
+        [TestMethod]
+        public void Split_WhenUsingQuotedString_IgnoresSeperator()
+        {
+            const string INPUT = "\"just, a string\",seperated";
+            const string SEPERATOR = ",";
+
+            string[] result = ColumnSplitter.Split(INPUT, SEPERATOR);
+
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual("\"just, a string\"", result[0]);
+            Assert.AreEqual("seperated", result[1]);
+        }
+    }
+}
