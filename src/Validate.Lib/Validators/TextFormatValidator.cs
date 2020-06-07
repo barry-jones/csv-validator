d4c8b447e7fd6b6a0612493139346048dd4f4afe
Barry Jones <github@barryjones.me.uk>
2020-06-07 22:43:44 +0100

    added format to textvalidator error message

--- src/Validate.Lib/Validators/TextFormatValidator.cs
+++ src/Validate.Lib/Validators/TextFormatValidator.cs
@@ -6,10 +6,12 @@ namespace FormatValidator.Validators
     internal class TextFormatValidator : ValidationEntry
     {
         private Regex _format;
+        private string _originFormatString;
 
         public TextFormatValidator(string format)
         {
             _format = new Regex(format);
+            _originFormatString = format;
         }
 
         public override bool IsValid(string toCheck)
@@ -17,7 +19,7 @@ namespace FormatValidator.Validators
             bool isValid = string.IsNullOrWhiteSpace(toCheck) || _format.IsMatch(toCheck);
             if(!isValid)
             {
-                base.Errors.Add(new ValidationError(0, string.Format("String '{0}' was not in correct format.", toCheck)));
+                base.Errors.Add(new ValidationError(0, string.Format("String '{0}' was not in correct format [{1}].", toCheck, _originFormatString)));
             }
             return isValid;
         }
